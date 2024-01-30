e75f5732d314fd4166f096995e74dad3c9076000
nessmamd <113803210+nessmamd@users.noreply.github.com>
2024-01-30 11:30:05 -0700

    ok

---
+++ random_1.py
@@ -0,0 +1,13 @@
+from github import Github
+# or using an access token
+g = Github("your_access_token")
+repo = g.get_repo("username/repo")
+
+## Complete your tasks from herefrom github import Github
+# or using an access token
+
+g = Github("your_access_token")
+repo = g.get_repo("username/repo")
+
+
+## Complete your tasks from here
